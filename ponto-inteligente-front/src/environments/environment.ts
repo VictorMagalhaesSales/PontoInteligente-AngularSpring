877d9d22e9639e0e9680bc605f2f234424fafe7e
VictorMagalhaesSales <victormagalhaessales@gmail.com>
2018-12-16 00:24:31 -0300

    Direcionando as requisições para o mesmo domínio(CORS)

--- ponto-inteligente-front/src/environments/environment.ts
+++ ponto-inteligente-front/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   baseUrl: 'http://localhost:8080/',
-  baseApiUrl: 'http://localhost:8080/api/'
+  baseApiUrl: 'http://localhost:4200/api/'
 };
 
 /*
